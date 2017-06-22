ae4bc8978d12f261d6e527ea205954780bc0f569
Niranjan Kala <niranjan.kala@gmail.com>
2017-06-23 04:17:28 +0530

    -

---
+++ MySqlEF6Integration/MySqlEF6Integration.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MySqlEF6Integration.UI.MvcApplication" Language="C#" %>
---
+++ MySqlEF6Integration/MySqlEF6Integration.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySqlEF6Integration.UI.Startup))]
+namespace MySqlEF6Integration.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
